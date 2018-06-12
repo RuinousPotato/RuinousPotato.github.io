5d1603d7362dd690cc887c7993e65f41fc63734a
Ruinous Potato <fusiek@gmail.com>
2018-06-12 15:41:21 +0200

    robofriends added

---
+++ robofriends/src/robots.js
@@ -0,0 +1,68 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Juliusz Wolny',
+    username: 'Julek',
+    email: 'powolny@dumm.is'
+  },
+  {
+    id: 2,
+    name: 'Dorota Niezdecydowana',
+    username: 'Dotka',
+    email: 'dotka@yumcia.tv'
+  },
+  {
+    id: 3,
+    name: 'John Waker',
+    username: 'dragon',
+    email: 'green@grass.php'
+  },
+  {
+    id: 4,
+    name: 'Wojciech Ojboli',
+    username: 'whitechristmas69',
+    email: 'whitetr45h@nfz.info'
+  },
+  {
+    id: 5,
+    name: 'Marek Prawiczek',
+    username: 'Penetrator123',
+    email: 'piast@lech.de'
+  },
+  {
+    id: 6,
+    name: 'Marcin Krystian',
+    username: 'wyznawca',
+    email: 'kapitan@polska.pl'
+  },
+  {
+    id: 7,
+    name: 'Antoni Likwidator',
+    username: 'Tortimandi',
+    email: 'wot@mon.gov'
+  },
+  {
+    id: 8,
+    name: 'Marek Lewak',
+    username: 'Midgetlover',
+    email: 'more@bitcoin.biz'
+  },
+  {
+    id: 9,
+    name: 'Andrzej Ministrant',
+    username: 'Snickers',
+    email: 'daj.mnie@dlugopis.org'
+  },
+  {
+    id: 10,
+    name: 'Jarosław Wielki',
+    username: 'Miszcz',
+    email: 'jarek@cat.net'
+  },
+  {
+    id: 11,
+    name: 'Wacław Nabrzmiały',
+    username: 'Wacek',
+    email: 'waclaw@cat.net'
+  }
+];
